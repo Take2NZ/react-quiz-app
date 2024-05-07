1d0e4790e86324697ed5f9e4997c154b98b3c759
Josh Garth <jpgarth@gmail.com>
2024-05-07 12:04:03 +1000

    Initial commit

---
+++ src/Questions.js
@@ -0,0 +1,23 @@
+// By convention, the first answer is always
+// the correct one. We'll shuffle them before
+// displaying them.
+const questions = [
+  {
+    question: 'What is React?',
+    answers: [
+      "It's a framework",
+      "It's a language",
+      "It's like Facebook"
+    ]
+  },
+  {
+    question: 'What is JavaScript?',
+    answers: [
+      "It's a framework",
+      "It's a language",
+      "It's the same as Java",
+    ]
+  }
+]
+
+export default questions;
